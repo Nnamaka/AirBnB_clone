bfaa80c40af1eac6ecd615e5d33d5f5739e504a1
nnamaka <nnamaka7@gmail.com>
2023-02-11 15:46:23 -0800

    added class name

--- models/base_model.py
+++ models/base_model.py
@@ -47,5 +47,5 @@ class BaseModel:
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.
+        clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
